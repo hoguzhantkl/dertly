638b532bd1ec6759857cb050394bbf05a4e8adaf
alperenf <fatihbab34@gmail.com>
2022-12-29 18:03:56 +0300

    Integrated Firebase Authentication and Firestore

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,22 @@
+import 'dart:async';
+
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
+
 import 'package:flutter/material.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+
   runApp(const App());
 }
 
 class App extends StatelessWidget {
   const App({super.key});
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
